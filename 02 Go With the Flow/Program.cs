b28ceeb903987b619b0b7189f2b7cd5e50c2c360
Karn Sharma <karn.sharma024@gmail.com>
2016-07-19 15:31:45 -0400

    Final Version

---
+++ 02 Go With the Flow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Go_With_the_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Are you feeling hungry?");
+            string answer = Console.ReadLine();
+           
+
+
+
+            if (answer == "Yes")
+                Console.WriteLine("Ok here you go have a Snickers bar!");
+            else if (answer == "No")
+                Console.WriteLine("Oh ok well sorry for bothering you, have a good day!");
+            else if (answer == "Maybe")
+                Console.WriteLine("Hmm well maybe you should have a glass of water and I'll come back and ask you later.");
+           
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
