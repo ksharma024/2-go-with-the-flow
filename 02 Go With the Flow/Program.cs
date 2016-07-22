fed4a40cc7c3515faf44cfd386d98f4c8316c9c6
Karn Sharma <karn.sharma024@gmail.com>
2016-07-22 09:50:51 -0400

    Added Loops

--- 02 Go With the Flow/Program.cs
+++ 02 Go With the Flow/Program.cs
@@ -10,16 +10,37 @@ namespace _02_Go_With_the_Flow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! Are you feeling hungry?");
+            Console.WriteLine("Hello! Are you feeling hungry? (Yes/No/Maybe)");
             string answer = Console.ReadLine();
-           
 
+            //Answer "Yes" or "No"
 
 
             if (answer == "Yes")
-                Console.WriteLine("Ok here you go have a Snickers bar!");
+            {
+                Console.WriteLine("Ok would you like to eat something healthy or junk food?");
+                string food = Console.ReadLine();
+
+                if (food == "healthy")
+                    Console.WriteLine("Great choice! Here is an apple!");
+
+                else if
+                    (food == "junk food")
+                    Console.WriteLine("Fine it's your body after all...here have an ice cream sundae.");
+            }
+
+
             else if (answer == "No")
-                Console.WriteLine("Oh ok well sorry for bothering you, have a good day!");
+            {
+                Console.WriteLine("Oh are you sure? (Yes/No)");
+                string reply = Console.ReadLine();
+
+                if (reply == "Yes")
+                    Console.WriteLine("Ok, I won't ask you again, have a nice day!");
+                else if (reply == "No")
+                    Console.WriteLine("Ok well think about and come find me.");
+            }
+
             else if (answer == "Maybe")
                 Console.WriteLine("Hmm well maybe you should have a glass of water and I'll come back and ask you later.");
            
