473126ad3360f49a0df5a2c74b309eae4113cf47
Karn Sharma <karn.sharma024@gmail.com>
2016-08-03 14:38:36 -0400

    Add Loop to Existing Project

--- 02 Go With the Flow/Program.cs
+++ 02 Go With the Flow/Program.cs
@@ -10,43 +10,45 @@ namespace _02_Go_With_the_Flow
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! Are you feeling hungry? (Yes/No/Maybe)");
-            string answer = Console.ReadLine();
-
-            //Answer "Yes" or "No"
-
-
-            if (answer == "Yes")
+            while (true)
             {
-                Console.WriteLine("Ok would you like to eat something healthy or junk food?");
-                string food = Console.ReadLine();
+                Console.WriteLine("Hello! Are you feeling hungry? (Yes/No/Maybe)");
+                string answer = Console.ReadLine();
 
-                if (food == "healthy")
-                    Console.WriteLine("Great choice! Here is an apple!");
+                //Answer "Yes" or "No"
 
-                else if
-                    (food == "junk food")
-                    Console.WriteLine("Fine it's your body after all...here have an ice cream sundae.");
+
+                if (answer == "Yes")
+                {
+                    Console.WriteLine("Ok would you like to eat something healthy or junk food?");
+                    string food = Console.ReadLine();
+
+                    if (food == "healthy")
+                        Console.WriteLine("Great choice! Here is an apple!");
+
+                    else if
+                        (food == "junk food")
+                        Console.WriteLine("Fine it's your body after all...here have an ice cream sundae.");
+                }
+
+
+                else if (answer == "No")
+                {
+                    Console.WriteLine("Oh are you sure? (Yes/No)");
+                    string reply = Console.ReadLine();
+
+                    if (reply == "Yes")
+                        Console.WriteLine("Ok, I won't ask you again, have a nice day!");
+                    else if (reply == "No")
+                        Console.WriteLine("Ok well think about and come find me.");
+                }
+
+                else if (answer == "Maybe")
+                    Console.WriteLine("Hmm well maybe you should have a glass of water and I'll come back and ask you later.");
+
+
+                Console.ReadKey();
             }
-
-
-            else if (answer == "No")
-            {
-                Console.WriteLine("Oh are you sure? (Yes/No)");
-                string reply = Console.ReadLine();
-
-                if (reply == "Yes")
-                    Console.WriteLine("Ok, I won't ask you again, have a nice day!");
-                else if (reply == "No")
-                    Console.WriteLine("Ok well think about and come find me.");
-            }
-
-            else if (answer == "Maybe")
-                Console.WriteLine("Hmm well maybe you should have a glass of water and I'll come back and ask you later.");
-           
-
-            Console.ReadKey();
-
 
 
 
